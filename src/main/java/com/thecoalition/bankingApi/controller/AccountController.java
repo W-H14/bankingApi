545376ab6144646c5cff98b6f5191fcfb4ecfb7f
henry santos <reycara7812@gmail.com>
2023-11-15 12:00:15 -0500

    Adds controller

---
+++ src/main/java/com/thecoalition/bankingApi/controller/AccountController.java
@@ -0,0 +1,46 @@
+package com.thecoalition.bankingApi.controller
+import com.thecoalition.bankingApi.model.Account;
+import com.thecoalition.bankingApi.repository.AccountRepository;
+import com.thecoalition.bankingApi.repository.AccountRepository;
+import com.thecoalition.bankingApi.service.AccountService;
+
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Optional;
+
+public class AccountController {
+
+    @Autowired
+    private AccountRepository accountRepo;
+
+    @Autowired
+    private  AccountService accountService;
+
+
+
+    public void verifyCostumer(Long CostumerId) throws ResourceNotFoundException {
+        Optional<Costumer> costumer = costumerService.getCostumer(CostumerId);
+        if (Costumer.isEmpty()) {
+            throw new ResourceNotFoundException("Costumer with id " + CostumerId + " not found");
+        }
+    }
+
+
+
+    @RequestMapping(value = "/costumers/{costumerId}/accounts", method = RequestMethod.POST)
+    public ResponseEntity<?> createAccount (@PathVariable Long costumerId, @RequestBody Account account) {
+        verifyCostumer(costumerId);
+        accountService.createAccount(costumerId, account);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(account.getAccountId()).toUri());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
--- src/main/java/com/thecoalition/bankingApi/service/AccountService.java
+++ src/main/java/com/thecoalition/bankingApi/service/AccountService.java
@@ -1,4 +1,53 @@
 package com.thecoalition.bankingApi.service;
 
-public class AccountService {
-}
+import com.thecoalition.bankingApi.model.Account;
+import com.thecoalition.bankingApi.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+
+    @Service
+    public class AccountService {
+
+        @Autowired
+        AccountRepository AccountRepo;
+
+        @Autowired
+
+        public void getAllAccounts() {
+            Iterable<Account> allAccounts = AccountRepo.findAll();
+        }
+
+        public void createAccount(Account account) {
+            AccountRepo.save(account);
+        }
+
+        public Optional<Account> getAccount(Long AccountId) {
+            return AccountRepo.findById(AccountId);
+        }
+
+        public Account updateAccount(Account updatedAccount, Long accountId) {
+            // Save the entity
+            Optional<Account> accountOptional = AccountRepo.findById(accountId);
+            // if (AccountOptional.isEmpty()) {
+            //     throw new ResourceNotFoundException(;
+            //  }
+            Account existingAccount = accountOptional.get();
+
+            existingAccount.setBalance(updatedAccount.getBalance());
+            existingAccount.setNickname(updatedAccount.getNickname());
+            existingAccount.setType(updatedAccount.getType());
+            existingAccount.setRewardPoints(updatedAccount.getRewardPoints());
+
+
+            return existingAccount;
+        }
+
+        public void deleteAccount(Long accountId) {
+            AccountRepo.deleteById(accountId);
+        }
+    }
+
