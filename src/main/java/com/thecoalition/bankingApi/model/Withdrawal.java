7cddb7815bf0d0f5de43925be9ba51e8c71d7a2d
SpaceTilmar <21tilmaro@gmail.com>
2023-11-15 12:20:44 -0500

    Updated files.

--- src/main/java/com/thecoalition/bankingApi/model/Withdrawal.java
+++ src/main/java/com/thecoalition/bankingApi/model/Withdrawal.java
@@ -1,4 +1,101 @@
 package com.thecoalition.bankingApi.model;
 
 public class Withdrawal {
+private Long id;
+private String type;
+private String transaction_date;
+private String status;
+private Long payer_id;
+private String medium;
+private Double amount;
+private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getPayer_id() {
+        return payer_id;
+    }
+
+    public void setPayer_id(Long payer_id) {
+        this.payer_id = payer_id;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Withdrawal(Long id, String type, String transaction_date, String status, Long payer_id, String medium, Double amount, String description) {
+        this.id = id;
+        this.type = type;
+        this.transaction_date = transaction_date;
+        this.status = status;
+        this.payer_id = payer_id;
+        this.medium = medium;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Withdrawal{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", transaction_date='" + transaction_date + '\'' +
+                ", status='" + status + '\'' +
+                ", payer_id=" + payer_id +
+                ", medium='" + medium + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
