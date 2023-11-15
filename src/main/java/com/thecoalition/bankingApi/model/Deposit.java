b5c8405adc0e98426fc98f10f71d13e55a931189
W-H14 <wyatthall390@gmail.com>
2023-11-15 12:19:56 -0500

    model

--- src/main/java/com/thecoalition/bankingApi/model/Deposit.java
+++ src/main/java/com/thecoalition/bankingApi/model/Deposit.java
@@ -1,10 +1,41 @@
 package com.thecoalition.bankingApi.model;
 
 import javax.persistence.*;
-
+import javax.validation.constraints.NotEmpty;
 @Entity
 public class Deposit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "")
+    @NotEmpty
+    @Column(name = "DEPOSIT_ID")
+    private Long depositID;
+
+    @Column(name = "DEPOSIT_TYPE")
+    @NotEmpty
+    private String type;
+    @Column(name = "TRANSACTION_DATE")
+    @NotEmpty
+    private String transaction_date;
+
+    @Column(name = "STATUS")
+    @NotEmpty
+    private String status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PAYEE_ID")
+    @NotEmpty
+    private Long payee_id;
+    @Column(name = "MEDIUM")
+    @NotEmpty
+    private String medium;
+    @Column(name = "AMOUNT")
+    @NotEmpty
+    private Double amount;
+    @Column(name = "DESCRIPTION")
+    @NotEmpty
+    private String description;
+
+
+
+
 }
