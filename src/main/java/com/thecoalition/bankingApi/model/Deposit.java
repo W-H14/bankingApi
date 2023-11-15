414fa0eb62e82d3289d5360330a218d39f652a82
W-H14 <wyatthall390@gmail.com>
2023-11-15 11:55:34 -0500

    inital

--- src/main/java/com/thecoalition/bankingApi/model/Deposit.java
+++ src/main/java/com/thecoalition/bankingApi/model/Deposit.java
@@ -1,4 +1,10 @@
 package com.thecoalition.bankingApi.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Deposit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "")
 }
