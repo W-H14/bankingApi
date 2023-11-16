41c13dbd39ac8859fd5809c1f019f51e5c489979
W-H14 <wyatthall390@gmail.com>
2023-11-16 12:57:19 -0500

    adds rest of service

--- src/main/java/com/thecoalition/bankingApi/controller/DepositController.java
+++ src/main/java/com/thecoalition/bankingApi/controller/DepositController.java
@@ -1,2 +1,42 @@
-package com.thecoalition.bankingApi.controller;public class DepositController {
+package com.thecoalition.bankingApi.controller;
+
+import com.thecoalition.bankingApi.model.Account;
+import com.thecoalition.bankingApi.model.Deposit;
+import com.thecoalition.bankingApi.service.AccountService;
+import com.thecoalition.bankingApi.service.DepositService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+public class DepositController {
+    @Autowired
+    private DepositService depositService;
+    @Autowired
+    private AccountService accountService;
+    @GetMapping("/accounts/{accountId}/deposits")
+    public Iterable<Deposit> getAll(@PathVariable Long accountId){
+        return depositService.getAllDeposits(accountId);
+    }
+    @GetMapping("/deposits/{depositId}")
+    public Optional<Deposit> getById(@PathVariable Long depositId){
+        return depositService.getDeposit(depositId);
+    }
+    @PostMapping("/accounts/{accountId}/deposits")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createDeposit(@PathVariable Long accountId, @RequestBody Deposit deposit){
+       depositService.createDeposit(accountId, deposit);
+    }
+    @PutMapping("/deposits/{depositId}")
+    public Deposit editDeposit(@PathVariable Long depositId, @RequestBody Deposit deposit){
+        return depositService.editDeposit(depositId, deposit);
+    }
+    @DeleteMapping("/deposit/{depositId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // Use HttpStatus.CREATED for 201 status
+    public void deleteDeposit(@Valid @PathVariable Long depositId) {
+        depositService.deleteDeposit(depositId);
+    }
 }
