c0426de807089631a95cb1a08ddca1f6380fd2cf
SpaceTilmar <21tilmaro@gmail.com>
2023-11-27 13:59:30 -0500

    Updated Withdrawal

--- src/main/java/com/thecoalition/bankingApi/repository/AccountActivityRepository.java
+++ src/main/java/com/thecoalition/bankingApi/repository/AccountActivityRepository.java
@@ -1,6 +1,7 @@
 package com.thecoalition.bankingApi.repository;
 
 import com.thecoalition.bankingApi.model.AccountActivity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccountActivityRepository extends CrudRepository<AccountActivity, Long> {
--- src/main/java/com/thecoalition/bankingApi/repository/WithdrawalRepository.java
+++ src/main/java/com/thecoalition/bankingApi/repository/WithdrawalRepository.java
@@ -1,11 +1,16 @@
 package com.thecoalition.bankingApi.repository;
 
+import com.thecoalition.bankingApi.model.Deposit;
 import com.thecoalition.bankingApi.model.Withdrawal;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface WithdrawalRepository extends CrudRepository<Withdrawal, Long> {
 
-    Iterable<Withdrawal> findAllById(Long accountId);
+    @Query(value = "SELECT d FROM Withdrawal d WHERE d.payer.id = :accountId",nativeQuery = true)
+    Optional<Deposit> findByPayerId(@Param("accountId") Long accountId);
 }
+
