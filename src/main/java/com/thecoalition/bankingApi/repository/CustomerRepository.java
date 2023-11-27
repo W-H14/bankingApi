10925e30ac353eeceb210ccb5206d6da93d4b77f
henry santos <reycara7812@gmail.com>
2023-11-27 14:12:55 -0500

    Adds costumer query

--- src/main/java/com/thecoalition/bankingApi/repository/CustomerRepository.java
+++ src/main/java/com/thecoalition/bankingApi/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.thecoalition.bankingApi.repository;
 
 import com.thecoalition.bankingApi.model.Account;
 import com.thecoalition.bankingApi.model.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +18,6 @@ public interface CustomerRepository extends CrudRepository<Customer, Long> {
 //    List<Customer>findByLastName(String lastName);
 //    List<Customer>findByFirstNameAndLastName(String firstName, String lastName);
 //    List<Customer> findByAddress(String city);
-
+@Query(value = "SELECT c FROM Customer c WHERE c.accountId = :accountId", nativeQuery = true)
+Iterable<Customer> findCustomersByAccountId(@Param("accountId") Long accountId);
 }
