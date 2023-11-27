af761ecb4e12ee9cebacc3570319e108f1bd3efd
NaiyaJS <NaiyaJS04@gmail.com>
2023-11-27 13:44:26 -0500

    fixed customer query in customerRepo

--- src/main/java/com/thecoalition/bankingApi/repository/CustomerRepository.java
+++ src/main/java/com/thecoalition/bankingApi/repository/CustomerRepository.java
@@ -2,19 +2,17 @@ package com.thecoalition.bankingApi.repository;
 
 import com.thecoalition.bankingApi.model.Account;
 import com.thecoalition.bankingApi.model.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    Iterable<Customer> findCustomersByAccountId(Long accountId);
 
-//   phone notes
-//    List<Customer> findByFirstName(String firstName);
-//    List<Customer>findByLastName(String lastName);
-//    List<Customer>findByFirstNameAndLastName(String firstName, String lastName);
-//    List<Customer> findByAddress(String city);
-
+    @Query(value = "SELECT c FROM Customer c WHERE c.accountId = :accountId", nativeQuery = true)
+    Iterable<Customer> findCustomersByAccountId(@Param("accountId") Long accountId);
+    
 }
